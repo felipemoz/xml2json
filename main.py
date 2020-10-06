2dae218e21294db6324cc86925bf558e3f082e93
Felipe Moz <fmoz@microsoft.com>
2020-10-06 18:07:43 -0300

    dockerfile

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/hello")
+@app.route("/health")
 def hello():
-    return "Hello, World!"
+    return "I'm alive!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host ='0.0.0.0', port=80)
